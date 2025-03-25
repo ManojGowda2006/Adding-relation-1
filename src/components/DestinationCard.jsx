a5a3c5514c70e62e1b2f278963b37f98d03772f3
ManojGowda2006 <manojgowdan807@gmail.com>
2025-03-25 12:14:13 +0530

    done

--- src/components/DestinationCard.jsx
+++ src/components/DestinationCard.jsx
@@ -5,10 +5,10 @@ function DestinationCard({ id, name, location, image, description, price }) {
   return (
     <div key={id} className="destination-card">
       <img src={image} className="destination-image" alt={name} />
-      <p className="destination-name">{name}</p>
-      <p className="destination-location">{location}</p>
-      <p className="destination-description">{description}</p>
-      <p className="destination-price">{price}</p>
+      <p className="destination-name">Name :{name}</p>
+      <p className="destination-location">Location :{location}</p>
+      <p className="destination-description">Description :{description}</p>
+      <p className="destination-price">Price : {price}</p>
     </div>
   );
 }
